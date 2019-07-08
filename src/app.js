c3bb9e17924c26f16d2cbffdc0e220a5f3e51e61
unknown <adriancab@gmail.com>
2019-07-08 00:01:57 -0700

    Setup app for Herkou

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
 const app = express()
+const port = process.env.PORT || 3000
 //set path to files
 
 // Define paths for Express config
@@ -109,7 +110,7 @@ app.get('*', (req,res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
 
